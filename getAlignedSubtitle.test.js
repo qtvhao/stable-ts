e26180a8918fbf1be64a68c271a5aab68a3e0d72
Your Name <you@example.com>
2024-08-28 17:00:55 +0200

    Add

--- getAlignedSubtitle.test.js
+++ getAlignedSubtitle.test.js
@@ -1,15 +1,18 @@
 let { getAlignedSubtitle } = require('./getAlignedSubtitle.js');
 let child_process = require('child_process');
+let removeMd = require('remove-markdown');
 let fs = require('fs');
+let path = require('path');
+console.log('='.repeat(300));
 
 function getInput(alignFile) {
+    let alignFilename = path.basename(alignFile);
     let alignFileContent = fs.readFileSync(alignFile, 'utf8');
     let alignFileParsed = JSON.parse(alignFileContent);
-    let texts = alignFileParsed.data.videoScript.map(x => x.text).join('.\n');
-    console.log('texts', texts);
-    let alignFileTxt = '/tmp/align-1484178056.txt';
+    let texts = alignFileParsed.data.videoScript.map(x => removeMd(x.text)).join('.\n');
+    let alignFileTxt = '/align-input/' + alignFilename + '.txt';
     fs.writeFileSync(alignFileTxt, texts);
-    let outputFile = '/tmp/output-1484178056.json';
+    let outputFile = '/tmp/output-' + alignFilename + '.json';
 
     return {
         alignFileTxt,
@@ -19,13 +22,7 @@ function getInput(alignFile) {
 }
 let model = 'tiny';
 let language = 'vi';
-test('test alignment', () => {
-    let {
-        alignFileTxt,
-        outputFile,
-        audio,
-    } = getInput('/align-input/align-1484178056.json');
-    let audioFile = '/app/storage/audio/synthesize-result-1484178056.mp3';
+function checkAligned(alignFileTxt, outputFile, audio, audioFile) {
     child_process.execFileSync('stable-ts', [
         audioFile, 
         '--model', model, 
@@ -39,21 +36,29 @@ test('test alignment', () => {
 
     let alignedSubtitle = JSON.parse(fs.readFileSync(outputFile, 'utf8'));
     let aligned = getAlignedSubtitle(audio, alignedSubtitle);
-    console.log('aligned', aligned);
+    // console.log('aligned', aligned);
     for (let i = 0; i < aligned.length; i++) {
         let alignedItem = aligned[i];
         expect(alignedItem.aligned).toBeDefined();
         // 
-        // console.log('alignedItem', alignedItem.aligned);
         let firstAligned = alignedItem.aligned[0];
         let lastAligned = alignedItem.aligned[alignedItem.aligned.length - 1];
-        console.log('firstAligned', firstAligned.start, 'lastAligned', lastAligned.end);
+        // console.log('firstAligned', firstAligned, 'lastAligned', lastAligned);
         let firstAlignedStart = firstAligned.start;
         let lastAlignedEnd = lastAligned.end;
-        if (firstAlignedStart === lastAlignedEnd && i !== aligned.length - 1) {
-            console.log('firstAlignedStart === lastAlignedEnd', firstAlignedStart, lastAlignedEnd);
+        if (firstAlignedStart === lastAlignedEnd) {
+            console.log('firstAlignedStart === lastAlignedEnd', firstAligned, lastAligned);
             throw new Error('firstAlignedStart === lastAlignedEnd on ' + i + ' / ' + aligned.length);
         }
     }
+}
+test('test alignment', () => {
+    let {
+        alignFileTxt,
+        outputFile,
+        audio,
+    } = getInput('/align-input/align-1484178056.json');
+    let audioFile = '/align-input/synthesize-result-1484178056.mp3';
+    checkAligned(alignFileTxt, outputFile, audio, audioFile)
     // expect(aligned).toBeDefined();
 });
