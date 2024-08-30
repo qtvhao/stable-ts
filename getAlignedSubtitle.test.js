553441fdf575e8ef1b0d14edadcde846166b037e
Your Name <you@example.com>
2024-08-30 15:40:11 +0200

    Add

--- getAlignedSubtitle.test.js
+++ getAlignedSubtitle.test.js
@@ -26,7 +26,66 @@ function getInput(alignFile) {
 }
 let model = process.env.STABLE_TS_MODEL;
 let language = 'vi';
+function cutAudioByLongestSilence(audioFile) {
+    // find longest silence in audio
+    let silenceFile = '/align-output/silence.txt';
+    child_process.execFileSync('ffmpeg', ['-i', audioFile, '-af', 'silencedetect=n=-50dB:d=0.5,ametadata=print:file=' + silenceFile, '-f', 'null', '-']);
+    let silence = fs.readFileSync(silenceFile, 'utf8');
+    console.log('silence', JSON.stringify(silence, null, 2));
+    let matcherStartEndDuration = /silence_start=(\d+(\.\d+)?).*?silence_end=(\d+(\.\d+)?).*?silence_duration=(\d+(\.\d+)?)/gis;
+    let matches = silence.matchAll(matcherStartEndDuration);
+    let silences = [];
+    for (let match of matches) {
+        silences.push({
+            start: parseFloat(match[1]),
+            end: parseFloat(match[3]),
+            duration: parseFloat(match[5]),
+        });
+    }
+
+    console.log('silences', JSON.stringify(silences, null, 2));
+    let longestSilence = silences.reduce((longest, current) => {
+        return current.duration > longest.duration ? current : longest;
+    }, silences[0]);
+    console.log('longestSilence', longestSilence);
+    let start = longestSilence.start;
+    // 
+    let audioFileMp3 = '/align-output/audio.mp3';
+    // convert to mp3
+    child_process.execFileSync('ffmpeg', ['-y', '-i', audioFile, audioFileMp3]);
+    // cut audio into 2 parts, from 0 to start, and start to end
+    let audioFilePart1 = '/align-output/audio-part1.mp3';
+    let audioFilePart2 = '/align-output/audio-part2.mp3';
+    child_process.execFileSync('ffmpeg', ['-y', '-i', audioFileMp3, '-ss', 0, '-to', start, '-c', 'copy', audioFilePart1]);
+    child_process.execFileSync('ffmpeg', ['-y', '-i', audioFileMp3, '-ss', start, '-c', 'copy', audioFilePart2]);
+
+    return {
+        audioFilePart1,
+        audioFilePart2,
+    };
+}
 function checkAligned(alignFileTxt, outputFile, audio, audioFile) {
+
+    let {
+        audioFilePart1,
+        audioFilePart2,
+    } = cutAudioByLongestSilence(audioFile);
+    let outputFilePart1 = '/align-output/output-part1.json';
+    // let outputFilePart2 = '/align-output/output-part2.json';
+    child_process.execFileSync('stable-ts', [
+        audioFilePart1,
+        '--model', model,
+        '--language', language,
+        // '--align', alignFileTxt,
+        '--overwrite',
+        '--output', outputFilePart1,
+        '-fw',
+    ], {
+        stdio: 'inherit',
+    });
+    console.log('done 1');
+
+    process.exit(0);
     child_process.execFileSync('stable-ts', [
         audioFile,
         '--model', model,
