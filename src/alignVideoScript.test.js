8754279f09b57fb1898932862ef130460d99a626
Your Name <you@example.com>
2024-09-05 08:49:19 +0200

    Add

--- src/alignVideoScript.test.js
+++ src/alignVideoScript.test.js
@@ -1,7 +1,48 @@
+let child_process = require('child_process');
+let fs = require('fs');
 let alignVideoScript = require('./alignVideoScript.js');
-test('test alignment video script', () => {
+async function getAudioMp3Duration(audioMp3) {
+    let stdout = await new Promise((resolve, reject) => {
+        child_process.execFile('ffprobe', [
+            audioMp3
+        ], {
+            encoding: 'utf8',
+            // stdio: 'pipe'
+        }, (err, stdout, stderr) => {
+            console.log('stdout', stdout);
+            console.log('stderr', stderr);
+            console.log('err', err);
+            fs.writeFileSync('/align-input/ffprobe-stderr.txt', stderr.toString().trim());
+            if (err) {
+                reject(err);
+            } else {
+                resolve(stderr.toString().trim());
+            }
+        });
+    });
+
+    //   Duration: 00:03:20.76, start: 0.046042, bitrate: 160 kb/s
+    let duration = stdout.match(/Duration: (\d+):(\d+):(\d+)\.(\d+)/);
+    let hours = parseInt(duration[1]);
+    let minutes = parseInt(duration[2]);
+    let seconds = parseInt(duration[3]);
+    let milliseconds = parseInt(duration[4]);
+    let totalSeconds = hours * 3600 + minutes * 60 + seconds + milliseconds / 1000;
+
+    return totalSeconds;
+}
+test('test alignment video script', async () => {
+    let audioMp3 = '/align-input/synthesize-result-1790617115.mp3';
+    let videoScript = require('/align-input/align-1790617115.json').data.videoScript;
+    // 
+    let audioMp3Duration = await getAudioMp3Duration(audioMp3);
+    console.log('audioMp3Duration', audioMp3Duration);
+    // 00:03:20.76
+    fs.writeFileSync('/align-input/logs.txt', "Total audio duration: " + audioMp3Duration + "\n");
+    process.exit(0);
+    // 
     alignVideoScript(
-        require('/align-input/align-1790617115.json').data.videoScript,
-        '/align-input/synthesize-result-1790617115.mp3'
+        videoScript,
+        audioMp3
     );
 });
