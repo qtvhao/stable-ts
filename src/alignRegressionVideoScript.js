bdd3d6ca8389b9f88f5377209c6edd5a6252fd89
Your Name <you@example.com>
2024-09-08 09:56:43 +0200

    Add

--- src/alignRegressionVideoScript.js
+++ src/alignRegressionVideoScript.js
@@ -1,8 +1,8 @@
 let getAudioMp3Duration = require('./getAudioMp3Duration');
+let getCorrectedVideoScriptItems = require('./getCorrectedVideoScriptItems');
 let fs = require('fs');
 let path = require('path');
 let alignInputDir = path.join(__dirname, '..', 'align-input');
-let alignOutputDir = path.join(__dirname, '..', 'align-output');
 
 async function alignRegressionVideoScript(videoScript, audioFile) {
     let beforeCutAudioDuration = await getAudioMp3Duration(audioFile);
@@ -12,10 +12,14 @@ async function alignRegressionVideoScript(videoScript, audioFile) {
     }
     fs.appendFileSync(logsFile, " \n\n-> Align video script - Total sections: " + videoScript.length + "\n");
     fs.appendFileSync(logsFile, " \n" + videoScript.map(x => "| " + x.text.slice(0, 100).replace(/\n/g, ' ')).join('\n') + '\n');
-    // 
     fs.appendFileSync(logsFile, "   - Before cut, audio mp3 duration: " + beforeCutAudioDuration + "s\n");
-    
-    return videoScript;
+
+    let segments = [];
+    let correctedVideoScriptItems = await getCorrectedVideoScriptItems(videoScript, segments);
+
+    return [
+        ...correctedVideoScriptItems,
+    ];
 }
 
 module.exports = alignRegressionVideoScript;
---
+++ src/getCorrectedVideoScriptItems.js
@@ -0,0 +1,5 @@
+async function getCorrectedVideoScriptItems(videoScript, segments) {
+    return videoScript.slice(0, 2);
+}
+
+module.exports = getCorrectedVideoScriptItems;
