58b6c7a23032bd48619a88a81e41f5752cf3ec1b
Your Name <you@example.com>
2024-09-08 19:15:46 +0200

    Add

--- src/getCorrectedVideoScriptItems.js
+++ src/getCorrectedVideoScriptItems.js
@@ -1,57 +1,26 @@
 let synthesizeAudio = require('./synthesizeAudio.js');
-let levenshtein = require('fast-levenshtein');
+let getSegmentsForVideoScriptItem = require('./getSegmentsForVideoScriptItem.js');
+function cutAudioFromTimestamp(audioFile, _timestamp) {
+    return audioFile;
+}
 async function getCorrectedVideoScriptItems(videoScript, audioFile) {
     let segments = await synthesizeAudio(audioFile, videoScript);
-    console.log(segments);
-    segments = segments.map((segment, i, self) => {
-        if (i === 0) {
-            return {
-                start: segment.start,
-                end: segment.end,
-                text: segment.text,
-                previousSilenceDuration: 0,
-            }
+    let correctedVideoScript = [];
+    let cutAudioFrom = 0;
+    for (let i = 0; i < videoScript.length; i++) {
+        let videoScriptItem = videoScript[i];
+        videoScriptItem.aligned = getSegmentsForVideoScriptItem(videoScriptItem, segments);
+        if (i > 1) {
+            break;
         }
-        return {
-            start: segment.start,
-            end: segment.end,
-            text: segment.text,
-            previousSilenceDuration: segment.start - self[i - 1].end,
-        };
-    });
-    // 
-    console.log(segments);
-    let bestMatchSegment = segments.map((segment, i, self) => {
-
-    }).reduce((best, current) => {
-        return best.levenshteinDistance < current.levenshteinDistance ? best : current;
-    }, {
-        levenshteinDistance: Infinity,
-        segmentsFromStart: [],
-    });
-    throw new Error('Not implemented');
-    let timestamp = 0;
-    let correctedVideoScriptItems = videoScript.slice(0, 2).map(x => {
-        let start = timestamp;
-        timestamp++;
-        let end = timestamp;
-        return {
-            start,
-            end,
-            text: x.headings.join(' '),
-            aligned: [
-                {
-                    start: 1,
-                    end: 2,
-                    text: x.headings.join(' '),
-                }
-            ]
-        };
-    });
+        cutAudioFrom = videoScriptItem.aligned.slice(-1)[0].end;
+        segments = segments.slice(videoScriptItem.aligned.length);
+        correctedVideoScript.push(videoScriptItem);
+    }
 
     return {
-        correctedVideoScriptItems, 
-        audioLeft: audioFile
+        audioLeft: cutAudioFromTimestamp(audioFile, cutAudioFrom),
+        correctedVideoScriptItems: correctedVideoScript
     };
 }
 
