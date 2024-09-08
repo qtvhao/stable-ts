4e0151d5a05a9cd494130bc2e2cbb8eb09820495
Your Name <you@example.com>
2024-09-08 07:49:14 +0200

    Add

---
+++ src/alignRegressionVideoScript.js
@@ -0,0 +1,8 @@
+async function alignRegressionVideoScript(videoScript, audioFile) {
+    // let audioDuration = await getAudioDuration(audioFile);
+    // let videoDuration = await getVideoDuration(videoFile);
+
+    return videoScript;
+}
+
+module.exports = alignRegressionVideoScript;
--- src/getCheckedAlignedVideoScript.js
+++ src/getCheckedAlignedVideoScript.js
@@ -1,4 +1,5 @@
 let child_process = require('child_process');
+let alignRegressionVideoScript = require('./alignRegressionVideoScript.js');
 
 async function getCheckedAlignedVideoScript(job, audioFile) {
     // convert to mp3
@@ -14,6 +15,25 @@ async function getCheckedAlignedVideoScript(job, audioFile) {
     });
     audioFile = audioFileMp3;
 
+    job.data.videoScript = await alignRegressionVideoScript(job.data.videoScript, audioFile);
+    let lastVideoScriptItem = job.data.videoScript.slice(-1)[0];
+    let lastVideoScriptItem_End = lastVideoScriptItem.end;
+    let lastVideoScriptItem_Start = lastVideoScriptItem.start;
+
+    if (typeof lastVideoScriptItem_Start === 'undefined' || typeof lastVideoScriptItem_End === 'undefined') {
+        throw new Error('The last video script item start and end are undefined. This is not allowed. Start: ' + lastVideoScriptItem_Start + ', End: ' + lastVideoScriptItem_End);
+    }
+
+    if (lastVideoScriptItem_Start === lastVideoScriptItem_End) {
+        throw new Error('The last video script item start and end are the same. This is not allowed. Start: ' + lastVideoScriptItem_Start + ', End: ' + lastVideoScriptItem_End);
+    } else {
+        console.log('All aligned');
+        console.log('videoScript', job.data.videoScript.map(x => {
+            return x.aligned.map(x => x.text).join('');
+        }).join('\n'.repeat(5)))
+    }
+
+    return job;
 }
 
 module.exports = getCheckedAlignedVideoScript;
