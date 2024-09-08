fef59b4bd8b945cca1f400f252bafc7d74820057
Your Name <you@example.com>
2024-09-08 11:21:56 +0200

    Add

--- src/alignRegressionVideoScript.js
+++ src/alignRegressionVideoScript.js
@@ -3,16 +3,19 @@ let getCorrectedVideoScriptItems = require('./getCorrectedVideoScriptItems');
 let fs = require('fs');
 let path = require('path');
 let alignInputDir = path.join(__dirname, '..', 'align-input');
-
-async function alignRegressionVideoScript(videoScript, audioFile) {
-    let beforeCutAudioDuration = await getAudioMp3Duration(audioFile);
+function logToFile(text) {
     let logsFile = path.join(alignInputDir, 'logs.txt');
     if (!fs.existsSync(logsFile)) {
         fs.writeFileSync(logsFile, '');
     }
-    fs.appendFileSync(logsFile, " \n\n-> Align video script - Total sections: " + videoScript.length + "\n");
-    fs.appendFileSync(logsFile, " \n" + videoScript.map(x => "| " + x.text.slice(0, 100).replace(/\n/g, ' ')).join('\n') + '\n');
-    fs.appendFileSync(logsFile, "   - Before cut, audio mp3 duration: " + beforeCutAudioDuration + "s\n");
+    fs.appendFileSync(logsFile, text);
+}
+
+async function alignRegressionVideoScript(videoScript, audioFile) {
+    let beforeCutAudioDuration = await getAudioMp3Duration(audioFile);
+    logToFile(" \n\n-> Align video script - Total sections: " + videoScript.length + "\n");
+    logToFile(" \n" + videoScript.map(x => "| " + x.text.slice(0, 100).replace(/\n/g, ' ')).join('\n') + '\n');
+    logToFile("   - Before cut, audio mp3 duration: " + beforeCutAudioDuration + "s\n");
 
     let {correctedVideoScriptItems, audioLeft} = await getCorrectedVideoScriptItems(videoScript, audioFile);
 
@@ -21,8 +24,8 @@ async function alignRegressionVideoScript(videoScript, audioFile) {
         return correctedVideoScriptItems;
     }
 
-    fs.appendFileSync(logsFile, "   - Corrected video script items: " + correctedVideoScriptItems.length + "\n");
-    fs.appendFileSync(logsFile, "   - Incorrected video script items: " + incorrectedVideoScriptItems.length + "\n");
+    logToFile("   - Corrected video script items: " + correctedVideoScriptItems.length + "\n");
+    logToFile("   - Incorrected video script items: " + incorrectedVideoScriptItems.length + "\n");
 
     let othersAligned = await alignRegressionVideoScript(incorrectedVideoScriptItems, audioLeft);
 
