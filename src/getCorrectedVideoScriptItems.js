596880a0cb749c2ba0b38654d08ba45ca2195af4
Your Name <you@example.com>
2024-09-08 18:03:15 +0200

    Add

--- src/getCorrectedVideoScriptItems.js
+++ src/getCorrectedVideoScriptItems.js
@@ -1,6 +1,35 @@
 let synthesizeAudio = require('./synthesizeAudio.js');
+let levenshtein = require('fast-levenshtein');
 async function getCorrectedVideoScriptItems(videoScript, audioFile) {
     let segments = await synthesizeAudio(audioFile, videoScript);
+    console.log(segments);
+    segments = segments.map((segment, i, self) => {
+        if (i === 0) {
+            return {
+                start: segment.start,
+                end: segment.end,
+                text: segment.text,
+                previousSilenceDuration: 0,
+            }
+        }
+        return {
+            start: segment.start,
+            end: segment.end,
+            text: segment.text,
+            previousSilenceDuration: segment.start - self[i - 1].end,
+        };
+    });
+    // 
+    console.log(segments);
+    let bestMatchSegment = segments.map((segment, i, self) => {
+
+    }).reduce((best, current) => {
+        return best.levenshteinDistance < current.levenshteinDistance ? best : current;
+    }, {
+        levenshteinDistance: Infinity,
+        segmentsFromStart: [],
+    });
+    throw new Error('Not implemented');
     let timestamp = 0;
     let correctedVideoScriptItems = videoScript.slice(0, 2).map(x => {
         let start = timestamp;
