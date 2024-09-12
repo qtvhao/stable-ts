c6d3153280f91bc0d57b5c69d8084d0d7047791c
Your Name <you@example.com>
2024-09-12 11:25:02 +0200

    Add

--- src/getCorrectedVideoScriptItems.test.js
+++ src/getCorrectedVideoScriptItems.test.js
@@ -1,9 +1,10 @@
+let removeSpecialCharacters = require('./removeSpecialCharacters.js');
 let getCorrectedVideoScriptItems = require('./getCorrectedVideoScriptItems.js');
 let path = require('path');
 let testdataDir = path.join(__dirname, '..', 'testdata', 'corrected');
 let testDataProviders = [
     "2.json",
-    "100.json",
+    // "100.json",
 ];
 let fs = require('fs');
 let convertAudioFileMp3 = require('./convertAudioFileMp3.js')
@@ -21,9 +22,21 @@ testDataProviders.forEach(testDataProvider => {
         let {correctedVideoScriptItems} = await getCorrectedVideoScriptItems(videoScript, copyTo);
         for (let i = 0; i < correctedVideoScriptItems.length; i++) {
             let correctedVideoScriptItem = correctedVideoScriptItems[i];
-            let text = correctedVideoScriptItem.text;
+            // let text = correctedVideoScriptItem.text;
             let aligned = correctedVideoScriptItem.aligned;
-            // console.log(JSON.stringify(correctedVideoScriptItems[i], null, 2));
+            // console.log(JSON.stringify(correctedVideoScriptItem, null, 2));
+            let text = removeSpecialCharacters(videoScript[i].text).trim();
+            let firstSegment = aligned[0];
+            let lastSegment = aligned.slice(-1)[0];
+            // 
+            let firstSegment_5Words = firstSegment.words.slice(0, 5).map(x => x.word).join('').trim();
+            let lastSegment_5Words = lastSegment.words.slice(-5).map(x => x.word).join('').trim();
+            let alignedWords = aligned.map(x => x.text).join(' ').trim();
+            // 
+            console.log('='.repeat(10), alignedWords, '='.repeat(10), firstSegment_5Words, '='.repeat(10), text, '='.repeat(10), lastSegment_5Words);
+            expect(firstSegment_5Words).toBe(text.slice(0, firstSegment_5Words.length).trim());
+            expect(lastSegment_5Words).toBe(text.slice(-lastSegment_5Words.length).trim());
+
         }
     });
 });
