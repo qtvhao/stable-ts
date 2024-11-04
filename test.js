d0a7ef2a164ea30a999e7bf2f91ded79dd81c83c
Your Name <you@example.com>
2024-11-04 08:37:20 +0100

    Refactor test.js to add nestedMapObjects function for cleaning token properties before logging

--- test.js
+++ test.js
@@ -5,12 +5,37 @@ let data = {
   },
 };
 let fs = require('fs');
+function nestedMapObjects(tokens) {
+    if (tokens.raw) {
+        delete tokens.raw;
+    }
+    if (tokens.task) {
+        delete tokens.task;
+    }
+    if (tokens.loose) {
+        delete tokens.loose;
+    }
+
+    if (tokens.tokens) {
+        tokens.tokens = tokens.tokens.map(nestedMapObjects);
+    }
+    if (tokens.items) {
+        tokens.items = tokens.items.map(nestedMapObjects);
+    }
+    if (Array.isArray(tokens)) {
+        tokens = tokens.map(nestedMapObjects);
+    }
+
+    return tokens;
+}
 
 (async function() {
     let tJson = 't.json'
     let job = fs.readFileSync(tJson, 'utf8');
     let job1 = JSON.parse(job)[1];
-    console.log(JSON.parse(job1));
+    let tokens = (JSON.parse(job1).result.tokens);
+    tokens = nestedMapObjects(tokens);
+    console.log(JSON.stringify(tokens, null, 2));
     return;
     // let result =
     let response = await fetch(url, {
