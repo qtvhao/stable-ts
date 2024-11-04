753c527172df2ac25476100c7e258200e01da2fd
Your Name <you@example.com>
2024-11-04 10:21:39 +0100

    Refactor test.js to add functions for processing list items and removing markdown from text

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ let data = {
     text: ''
   },
 };
+const removeMd = require('remove-markdown');
 let fs = require('fs');
 function nestedMapObjects(tokens) {
     if (tokens.raw) {
@@ -28,13 +29,42 @@ function nestedMapObjects(tokens) {
 
     return tokens;
 }
+function getTextFromListItem(list_item) {
+    let text = list_item.text;
+    return {
+        text: removeMd(text),
+    }
+}
+function getSegmentsOfList(token) {
+    let items = token.items;
+    items = items.map(getTextFromListItem);
+
+    return items;
+}
 
 (async function() {
+    let audioFile = 'synthesize-result-2532432836.aac';
     let tJson = 't.json'
     let job = fs.readFileSync(tJson, 'utf8');
     let job1 = JSON.parse(job)[1];
     let tokens = (JSON.parse(job1).result.tokens);
     tokens = nestedMapObjects(tokens);
+    tokens = tokens.reduce((acc, token) => {
+        if (token.type === 'space') {
+            acc.push('='.repeat(251));
+        }else{
+            if ("list" === token.type) {
+                acc = acc.concat(getSegmentsOfList(token));
+            }else{
+                acc.push({
+                    type: (token.type),
+                    text: (token.text),
+                });
+            }
+        }
+        return acc;
+    }, []);
+
     console.log(JSON.stringify(tokens, null, 2));
     return;
     // let result =
